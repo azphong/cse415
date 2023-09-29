26700b8de39fc036bf48b70afd12b2dd9859186f
Aaron Hong <ahong02@uw.edu>
2023-09-29 14:18:21 -0700

    worked on a1_exercises

--- a1-starter-code/a1_exercises.py
+++ a1-starter-code/a1_exercises.py
@@ -1,20 +1,38 @@
+import math
 def is_a_triple(n):
     """Return True if n is a multiple of 3; False otherwise."""
+    return n % 3 == 0
 
 def last_prime(m):
     """Return the largest prime number p that is less than or equal to m.
     You might wish to define a helper function for this.
     You may assume m is a positive integer."""
+    while (not is_prime(m)):
+        m -= 1
+    return m
+    
+def is_prime(m):
+    n = m - 1
+    while (n < 1):
+        if (m % n == 0):
+            return False
+        n -= 1
+    return True
 
 def quadratic_roots(a, b, c):
     """Return the roots of a quadratic equation (real cases only).
     Return results in tuple-of-floats form, e.g., (-7.0, 3.0)
     Return "complex" if real roots do not exist."""
+    if (((b*b) - (4*a*c)) < 0):
+        return "complex"
+    return ((-1*b + math.sqrt(b*b - 4*a*c))/(2*a), (-1*b - math.sqrt(b*b - 4*a*c))/(2*a))
+    
 
 def new_quadratic_function(a, b, c):
     """Create and return a new, anonymous function (for example
     using a lambda expression) that takes one argument x and 
     returns the value of ax^2 + bx + c."""
+    return lambda x: a*x*x + b*x + c
 
 def perfect_shuffle(even_list):
     """Assume even_list is a list of an even number of elements.
@@ -22,6 +40,13 @@ def perfect_shuffle(even_list):
     Perfect shuffle means splitting a list into two halves and then interleaving
     them. For example, the perfect shuffle of [0, 1, 2, 3, 4, 5, 6, 7] is
     [0, 4, 1, 5, 2, 6, 3, 7]."""
+    first_half = even_list[:even_list.len()/2]
+    second_half = even_list[even_list.len()/2:]
+    result = []
+    for x in range (first_half.len()):
+        result.append(first_half[x])
+        result.append(second_half[x])
+    return result
 
 def list_of_3_times_elts_plus_1(input_list):
     """Assume a list of numbers is input. Using a list comprehension,
