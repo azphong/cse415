02ef3478bfe97ffddea1e48d18f81c51ef056460
Aaron Hong <ahong02@uw.edu>
2023-10-15 11:49:09 -0700

    worked on problem 1

--- a2-starter-code/Farmer_Fox.py
+++ a2-starter-code/Farmer_Fox.py
@@ -13,4 +13,58 @@ the Farmer, Fox, Chicken, and Grain.
 # Be sure your name, uwnetid, and 7-digit student number are given above in 
 # the format shown.
 
-pass  # replace this.
+LEFT=0
+RIGHT=1
+
+class State():
+
+    def __init__(self, d=None):
+        if d==None:
+            d = {'farmer':LEFT,
+                 'fox':LEFT,
+                 'chicken':LEFT,
+                 'grain':LEFT}
+        self.d=d
+
+    def __eq__(self,s2):
+        for thing in self.d:
+            if self.d[thing] != s2.d[thing]: return False
+        return True
+    
+    def __str__(self):
+        txt = ""
+        for thing in self.d:
+            txt += thing + " is on the "
+            if self.d[thing] == LEFT:
+                txt += "left\n"
+            else:
+                txt += "right\n"
+        return txt
+    
+    def __hash__(self):
+        return (self.__str__()).__hash__()
+    
+    def copy(self):
+        new_state = State({})
+        for thing in self.d:
+            new_state.d[thing] = self.d[thing]
+        return new_state
+    
+    def can_move(self,things):
+        farmer_side = []
+        for thing in self.d:
+            if self.d[thing]==self.d['farmer']:
+                farmer_side.append(thing)
+            if thing in things and thing not in farmer_side:
+                return False # can't move thing if not on same side as farmer
+        farmer_side.remove(thing for thing in things)
+        if 'chicken' in farmer_side and 'fox' in farmer_side:
+            return False # can't leave fox and chicken alone
+        if 'chicken' in farmer_side and 'grain' in farmer_side:
+            return False # can't leave chicken and grain alone
+        return True
+
+            
+
+
+#pass  # replace this.
